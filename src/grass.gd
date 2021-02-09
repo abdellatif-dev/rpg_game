6eba38836f0b2c6b68283edc42b08cacc9e1b2bd
abdellatif-dev <abdellatif.devlog@gmail.com>
2021-02-10 00:55:29 +0100

    killing grass with sword

--- src/grass.gd
+++ src/grass.gd
@@ -2,13 +2,16 @@ extends Node2D
 
 
 # warning-ignore:unused_argument
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("attack_attack"):
-		var GrassEffect = load("res://scenes/grass_effect.tscn")
+func death():
+	var GrassEffect = load("res://scenes/grass_effect.tscn")
 
-		var grass_effect = GrassEffect.instance()
+	var grass_effect = GrassEffect.instance()
 
-		var world = get_tree().current_scene
-		world.add_child(grass_effect)
-		grass_effect.global_position = global_position
-		queue_free()
+	var world = get_tree().current_scene
+	world.add_child(grass_effect)
+	grass_effect.global_position = global_position
+
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	death()
+	queue_free()
