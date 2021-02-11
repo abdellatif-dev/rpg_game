186d09782492c52e488790e69e540e9d3bfbf03a
abdellatif-dev <abdellatif.devlog@gmail.com>
2021-02-11 15:11:51 +0100

    roll state

--- src/player.gd
+++ src/player.gd
@@ -11,6 +11,7 @@ enum {
 }
 
 var state = move
+var roll_vector = Vector2.DOWN
 
 onready var animation_tree = $AnimationTree
 onready var animation_state = animation_tree.get("parameters/playback")
@@ -21,11 +22,14 @@ func _process(delta: float) -> void:
 		move:
 			move_state(delta)
 		roll:
-			pass
+			roll_state()
 		attack:
 			attack_state()
 
-func move_state(delta: float):
+func move_state(_delta: float):
+	input_to_move(_delta)
+
+func input_to_move(delta):
 	var velocity = Vector2.ZERO
 	var _input_vector = User_Input(Vector2.ZERO)
 	var movement = check_for_movement(_input_vector, velocity)
@@ -35,12 +39,16 @@ func move_state(delta: float):
 	animations(_input_vector)
 	if Input.is_action_just_pressed("attack_attack"):
 		state = attack
+	if Input.is_action_just_pressed("roll"):
+		state = roll
 
 func animations (_movement):
 	if _movement != Vector2.ZERO:
+		roll_vector = _movement
 		animation_tree.set("parameters/walk/blend_position", _movement)
 		animation_tree.set("parameters/idle/blend_position", _movement)
 		animation_tree.set("parameters/attack/blend_position", _movement)
+		animation_tree.set("parameters/roll/blend_position", _movement)
 		animation_state.travel("walk")
 	else:
 		animation_state.travel("idle")
@@ -62,5 +70,16 @@ func check_for_movement(input_vector: Vector2, velocity: Vector2):
 func attack_state():
 	animation_state.travel("attack")
 
-func finished_attack():
+func roll_state():
+	animation_state.travel("roll")
+	move()
+
+func move():
+	var velocity = Vector2.ZERO
+	var _input_vector = roll_vector * speed
+	var movement = check_for_movement(_input_vector, velocity)
+	move_and_slide(movement)
+
+func state_finished():
 	state= move
+
